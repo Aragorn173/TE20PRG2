8fd04ba61b29d1c08ecf38a2b62cf7a7e0350540
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2022-08-26 12:32:01 +0200

    gjorde kattis övningar

--- src/Kattis_hiss.java
+++ src/Kattis_hiss.java
@@ -3,19 +3,30 @@ import javax.swing.*;
 public class Kattis_hiss {
     public static void main(String[] args) {
         int x = 0;
-
+        int z = 0;
         String input = JOptionPane.showInputDialog("input");
-        for (int i = 0; i < input.length()-1; i++) {
-            if (input.charAt(i) == 's' && input.charAt(i+1) == 's') {
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == 's') {
+                z = i;
+                i =10000;
+            }
+        }
+        if (z == input.length()){
+            x = 0;
+        } else if (input.charAt(z) == 's' && input.charAt(z+1) == 's') {
+            x = 1;
+        }
+
+
+            /*if (input.charAt(z) == 's' && input.charAt(z+1) == 's') {
                 x = 1;
             } else {
                 x = 0;
+            }*/
+            if (x == 1) {
+                System.out.println("hiss");
+            } else {
+                System.out.println("no hiss");
             }
         }
-        if (x==1) {
-            System.out.println("hiss");
-        } else {
-            System.out.println("no hiss");
-        }
     }
-}
