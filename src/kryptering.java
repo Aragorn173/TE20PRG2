4cddfb5b4e87caa92458e25460cfd4b7d0696292
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2022-09-02 13:35:22 +0200

    nu använder den hexadecimal

--- src/kryptering.java
+++ src/kryptering.java
@@ -1,22 +1,24 @@
-import jdk.nashorn.internal.scripts.JO;
-
 import javax.swing.*;
 
 public class kryptering {
     public static void main(String[] args) {
         String input = JOptionPane.showInputDialog("vad vill du kryptera?");
-        String crypt = "";
-        char n = '(';
+        String krypt = "";
+        int n = '(';
+        String hex = "";
 
         for (int i = 0; i < input.length(); i++) {
-            char a = input.charAt(i);
-            if (a == ' '){
-                crypt += ' ';
-            } else {
-                crypt += (char)(a ^ n);
-            }
+            int a = input.charAt(i);
+            System.out.println(hex);
+            hex = Integer.toHexString(crypt(a,n));
+            krypt += hex;
         }
-        System.out.println(crypt);
+        System.out.println(krypt);
 
     }
+    static int crypt(int m, int k) {
+        int c = m ^ k;
+        return c;
+    }
 }
+
