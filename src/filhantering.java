c909efa4ebd5f37e0b0ea87ac357df4cf7b81250
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2022-09-08 13:43:36 +0200

    skapade någoting. lyckades inte läsa in en fil

---
+++ src/filhantering.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.ArrayList;
+
+import static java.awt.SystemColor.text;
+
+public class filhantering {
+    public static void main(String[] args) {
+        String filnamn = "info.txt";
+        ArrayList<String> text = new ArrayList<>();
+
+
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter("info.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BufferedWriter bw = new BufferedWriter(fw);
+        PrintWriter utFil = new PrintWriter(bw);
+        FileReader fr = null;
+
+
+        try {
+            fr = new FileReader("info.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader inFil = new BufferedReader(fr);
+
+        try {
+            String line = infill.readline();
+        }
+    }
+}
